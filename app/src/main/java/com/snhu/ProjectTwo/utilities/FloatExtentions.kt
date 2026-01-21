84b4b0b971de41feadd24eaf7bfad29a63f4292b
Cclark50 <Christian.clark5@snhu.edu>
2026-01-21 02:25:13 -0600

    Added const values for isValidWeight

--- app/src/main/java/com/snhu/ProjectTwo/utilities/FloatExtentions.kt
+++ app/src/main/java/com/snhu/ProjectTwo/utilities/FloatExtentions.kt
@@ -1,3 +1,8 @@
 package com.snhu.ProjectTwo.utilities
 
-fun Float.isValidWeight(): Boolean = this in 0.1f..3000f
+// Float extension that checks if our float is within the acceptable range
+// Const values let us change all instances of the upper and lower bounds from one location instead of
+// Needing to change it in every location they're referenced in
+private const val HIGHEST_WEIGHT: Float = 3000f
+private const val LOWEST_WEIGHT: Float = 0.1f
+fun Float.isValidWeight(): Boolean = this in LOWEST_WEIGHT..HIGHEST_WEIGHT
